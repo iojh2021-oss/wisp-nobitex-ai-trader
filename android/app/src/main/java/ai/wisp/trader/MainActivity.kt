0704ec0f014f047a7a5b9fbda4f5875e92f5d7f0
iojh2021-oss <iojh2021@gmail.com>
2026-08-23 18:10:45 +0330

    fix(android): opt into experimental Material3 TopAppBar

--- android/app/src/main/java/ai/wisp/trader/MainActivity.kt
+++ android/app/src/main/java/ai/wisp/trader/MainActivity.kt
@@ -52,6 +52,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
 @Composable
 private fun TraderApp() {
     var selectedTab by remember { mutableIntStateOf(0) }
